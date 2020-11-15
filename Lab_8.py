99a17f33be086ff90ab1657bf7c19e9eed5887a0
Igor Alekhnovych <igoraleh@gmail.com>
2020-11-15 15:24:06 +0000

    Word_Duplication
    Implemented WordDuplication class

--- Lab_8.py
+++ Lab_8.py
@@ -2,7 +2,6 @@
 # date: 13-11-2020
 # purpose: Lab 8
 
-# base class
 class WordGames:
     """
     Class to represent the word game's base class.
@@ -74,10 +73,30 @@ class WordGames:
         Returns: None.
         --------
         """
-        print("User input was: "+self.the_words)
+        print("User input was: " + self.the_words)
 
-class WordDuplication(WordGames): # you implement this and provide docstrings
-    pass
+class WordDuplication(WordGames): # you provide docstrings
+    def word_play(self):
+        # Print user input
+        super().word_play()
+
+        # First Type: Duplicate Characters
+        my_sentence = self.the_words
+        result_1 = ''.join([char * 2 for char in my_sentence])
+
+        # Second Type: Duplicate Words
+        my_sentence = self.the_words.strip().split()
+        my_list = []
+        for index, words in enumerate(my_sentence):
+            my_list.extend([my_sentence[index]] * 2)
+
+        # Join all the words with spaces
+        result_2 = ' '.join(my_list)
+
+        # Print Results
+        print('Duplication 1: ' + result_1)
+        print('Duplication 2: ' + result_2)
+        print('')
 
 class WordScramble(WordGames): # you implement this and provide docstrings
     pass
@@ -87,7 +106,9 @@ class WordScramble(WordGames): # you implement this and provide docstrings
 # if this class was a python module
 print(WordGames.__doc__)
 
-# Create an instances of the classes here:
-wg = WordGames()
-wg.word_play()
+# Create instances of the classes here:
+wd = WordDuplication()
+wd.word_play()
 
+ws = WordScramble()
+ws.word_play()
